5580cb73f56a0590a9327c9f70f1fed5d5c700f9
Shameer Rose <sham@Shameers-MacBook-Pro.local>
2023-11-27 15:20:46 -0500

    Add task creation

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ const taskList = document.getElementById("task-list");
 let taskItem = document.getElementsByClassName("task-item");
 let deleteButtons = document.getElementsByClassName("delete-button");
 
+const taskInput = document.getElementById("task-input");
+const inputButton = document.getElementById("input-button");
+
+function CreateTask(TaskName) {
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+
+    const taskDeleteButton = document.createElement("button");
+    taskDeleteButton.className = "delete-button";
+    taskDeleteButton.innerText = "X";
+    taskDeleteButton.addEventListener("click", function() {
+        this.parentElement.remove();
+    });
+    
+    const taskText = document.createElement("p");
+    taskText.innerHTML = TaskName;
+
+    taskList.appendChild(taskItem);
+    taskItem.appendChild(taskText);
+    taskItem.appendChild(taskDeleteButton);
+}
+
+// Create Tasks
+inputButton.addEventListener("click", function() {
+    CreateTask(taskInput.value);
+});
+
+// Delete Tasks
 for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].addEventListener("click", function() {
         this.parentElement.remove();
